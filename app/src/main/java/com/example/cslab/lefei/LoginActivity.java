21450023a34cefe1309637077c4e2ad4c443f3e7
mylvcs <wmy0828@nuaa.edu.cn>
2018-01-15 16:01:45 +0800

    Fixed bugs

--- app/src/main/java/com/example/cslab/lefei/LoginActivity.java
+++ app/src/main/java/com/example/cslab/lefei/LoginActivity.java
@@ -1,30 +1,46 @@
 package com.example.cslab.lefei;
 
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by CSLab on 2018/1/14.
  */
 
 public class LoginActivity extends AppCompatActivity{
-    private Button mButton;
-
+    private Button mRegisterButton;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        mButton.setOnClickListener(new View.OnClickListener() {
+        mRegisterButton= (Button)findViewById(R.id.register_button);
+        mLoginButton= (Button)findViewById(R.id.login_button);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
                                        @Override
                                        public void onClick(View view) {
-                                           Intent intent = new Intent(LoginActivity.this,SearchFlightActivity.class);
+                                           Toast.makeText(LoginActivity.this,R.string.Register,Toast.LENGTH_SHORT).show();
                                        }
                                    }
         );
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Loginintent =new Intent(LoginActivity.this, SearchFlightActivity.class);
+                startActivity(Loginintent);
+            }
+        });
     }
+
+
 }
