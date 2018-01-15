87da51ae9dd0f3c8c05f946c221f128168400bb6
mylvcs <wmy0828@nuaa.edu.cn>
2018-01-15 08:51:23 +0800

    Create Login

---
+++ app/src/main/java/com/example/cslab/lefei/RegisterActivity.java
@@ -0,0 +1,32 @@
+package com.example.cslab.lefei;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by CSLab on 2018/1/15.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button mButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+        mButton = (Button)findViewById(R.id.register_button);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View view) {
+                                           Intent intent = new Intent(RegisterActivity.this,SearchFlightActivity.class);
+                                           startActivity(intent);
+                                       }
+                                   });
+    }
+}
