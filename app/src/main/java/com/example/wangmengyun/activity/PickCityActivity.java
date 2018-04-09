8bda3da70c743d7903a25e4106b9368056dc7723
mylvcs <wmy0828@nuaa.edu.cn>
2018-04-09 16:08:12 +0800

    PickCity copy

---
+++ app/src/main/java/com/example/wangmengyun/activity/PickCityActivity.java
@@ -0,0 +1,8 @@
+package com.example.wangmengyun.activity;
+
+/**
+ * Created by wangmengyun on 2018/4/9.
+ */
+
+public class PickCityActivity {
+}
---
+++ app/src/main/java/com/example/wangmengyun/adapter/ContentAdapter.java
@@ -0,0 +1,16 @@
+package com.example.wangmengyun.adapter;
+
+/**
+ * Created by wangmengyun on 2018/4/9.
+ */
+
+public class ContentAdapter {
+    private static final ContentAdapter ourInstance = new ContentAdapter();
+
+    public static ContentAdapter getInstance() {
+        return ourInstance;
+    }
+
+    private ContentAdapter() {
+    }
+}
